84a697f25c6caa138161f0abf6e49bb21879060d
Mike Fiedler <miketheman@gmail.com>
2014-01-18 20:12:34 -0500

    style: AlignParameters

--- lib/chef/handler/datadog.rb
+++ lib/chef/handler/datadog.rb
@@ -57,12 +57,12 @@ class Chef
         begin
           # Send the Event data
           evt = @dog.emit_event(Dogapi::Event.new(event_data,
-            :msg_title => event_title,
-            :event_type => 'config_management.run',
-            :event_object => hostname,
-            :alert_type => alert_type,
-            :priority => event_priority,
-            :source_type_name => 'chef'
+                                                  :msg_title => event_title,
+                                                  :event_type => 'config_management.run',
+                                                  :event_object => hostname,
+                                                  :alert_type => alert_type,
+                                                  :priority => event_priority,
+                                                  :source_type_name => 'chef'
           ), :host => hostname)
 
           begin
